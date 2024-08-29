d2619aa4093e6975e5ef180105773d52ad584522
Devin Naicker <devinnaicker@gmail.com>
2024-08-28 21:02:08 -0700

    2

--- main.c
+++ main.c
@@ -1,35 +1,44 @@
-#include "main.h"
+#include "shell.h"
 
-#define PROMPT "$: "
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-int main(void) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-    while (1) {
-        printf(PROMPT);
-
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) {
-            if (feof(stdin)) {
-                printf("\n");
-                break;
-            } else {
-                perror("getline");
-                return (1);
-            }
-        }
-
-        line[strcspn(line, "\n")] = 0;
-
-        if (line[0] == '\0') {
-            continue;
-        }
-
-        fork_process(line);
-    }
-
-    free(line);
-    return 0;
-}i}
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
